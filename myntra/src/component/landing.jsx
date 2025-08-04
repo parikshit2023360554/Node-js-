f01d98c28126be376a50060e582c44f2620fb67b
Parikshit <parikshitsingh@Parikshits-MacBook-Air.local>
2025-08-04 10:35:39 +0530

    file added

---
+++ myntra/src/component/landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.css';
+
+const Landing = () => {
+  return (
+    <div className="landing-container">
+      {/* Your landing page content will go here */}
+    </div>
+  );
+};
+
+export default Landing;
